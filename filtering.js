107a5136d2b72db277d1558122fed4f822647407
Atifah28 <S1058932@connect.np.edu.sg>
2025-01-20 21:47:41 +0800

    added java script link at every page

---
+++ filtering.js
@@ -0,0 +1,5 @@
+const apiUrl = "https://database-9cfc.restdb.io/rest/menu";
+const apiKey = "677f336bc7a864b3d4c78324";
+
+// Fetch and apply filters
+document.getElementById('apply-filters').addEventListener('click',)
